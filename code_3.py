79a9a8389540ddece38d3c3c8cd73d7c44f8fd39
lepikakut <152447835+lepikakut@users.noreply.github.com>
2024-03-08 16:27:21 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        }
+        }
+    }
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+}
+        for (int item : data) {
